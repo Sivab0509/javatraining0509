9464e46dcae04a5ca59454ad533cd48011873310
sknabirasool <sknabirasool104@gmail.com>
2024-09-06 09:29:41 +0530

    saved

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println(" git Demo");
         System.out.println(" git Demo1");
+        System.out.println(" git Demo1");
     }
 }
