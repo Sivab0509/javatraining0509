b92e4d6cdad79304a02abe7b09c9d44514ca1e9e
sknabirasool <sknabirasool104@gmail.com>
2024-09-15 19:16:35 +0530

    update folder

--- src/main/java/org/example/Demo.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println(" git Demo");
-        System.out.println(" git Demo1");
-        System.out.println(" git Demo1");
-    }
-}
