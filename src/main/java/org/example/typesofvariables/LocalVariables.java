60172f0b30ee9f0ac6cd1eef09a48a62fded8dae
sknabirasool <sknabirasool104@gmail.com>
2024-09-15 19:19:37 +0530

    saved

--- src/main/java/org/example/typesofvariables/LocalVariables.java
+++ src/main/java/org/example/typesofvariables/LocalVariables.java
@@ -1,2 +1,35 @@
-package org.example.typesofvariables;public class LocalVariables {
+package org.example.typesofvariables;
+
+public class LocalVariables {
+
+    public  void calculateInterest()
+    {
+        int principle=1000; //local variable double
+        double rate=5.5;
+        double time=1;
+        double result=(principle*rate*time)/100;
+        System.out.println("Result "+result);
+    }
+
+    public  void oneMonth()
+    {
+        int principle=1000; //local variable double
+        double rate=5.5;
+        double time=1;
+        double result=(principle*rate*time)/100;
+        System.out.println("Result "+result);
+    }
+    public  void oneYear()
+    {
+        int principle=1000; //local variable double
+        double rate=5.5;
+        double time=1;
+        double result=(principle*rate*time)/100;
+        System.out.println("Result "+result);
+    }
+    public static void main(String[] args) {
+
+        LocalVariables obj=new LocalVariables();
+        obj.calculateInterest();
+    }
 }
